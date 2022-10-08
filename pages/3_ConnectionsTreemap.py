89814dcc96757aa485c85fae4571a492d19666c7
Ritesh Sharma <ritesh.sharma29@gmail.com>
2022-10-08 15:57:04 +0530

    Update 3_ConnectionsTreemap.py

--- pages/3_ConnectionsTreemap.py
+++ pages/3_ConnectionsTreemap.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Treemap of Company MyConnections Work@")
 
 st.sidebar.success("<Treemap> #  displays hierarchical data as a set of nested rectangles. Each group is represented by a rectangle, where area is proportional to its value.")
 
-connections_df = pd.read_csv(os.path.join(os.getcwd(),"Connections.csv"))
+connections_df = pd.read_csv(os.path.join(os.getcwd(),"pages/Connections.csv"))
 company_groupby = connections_df.groupby(by='Company').count().reset_index().sort_values(by='First Name', ascending=False).reset_index(drop=True)
 
 fig3 = px.treemap(company_groupby[:200], path=['Company', 'Position'],
