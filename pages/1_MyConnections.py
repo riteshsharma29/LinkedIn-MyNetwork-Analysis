78d84c1347a38b207d78368d9fcb6997dd73a593
Ritesh Sharma <ritesh.sharma29@gmail.com>
2022-10-08 15:39:07 +0530

    Update 1_MyConnections.py

--- pages/1_MyConnections.py
+++ pages/1_MyConnections.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="MyConnections")
 
 st.sidebar.success("<Connected On> indicates the date I connected to that person. Visualization is represented with Plotly Line chart.")
 
-connections_df = pd.read_csv("Connections.csv")
+connections_df = pd.read_csv("../Connections.csv")
 connections_df ["Connected On"] = pd.to_datetime(connections_df ["Connected On"])
 connections_line = px.line(connections_df.groupby(by='Connected On').count().reset_index(),
                            x="Connected On",
